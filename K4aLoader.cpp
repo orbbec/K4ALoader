05d2fc9d66a9bfb393a126ec5f72189d1ddf72e6
lingyi <lingyi@orbbec.com>
2023-12-05 18:56:36 +0800

    [k4a][feat]: 初始代码上传。

---
+++ K4aLoader.cpp
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "K4aLoader.h"
+
+static void deloader_init_once(deloader_global_context_t* global);
+static void deloader_deinit(void);
+
+// Creates a function called deloader_global_context_t_get() which returns the initialized
+// singleton global
+K4A_DECLARE_GLOBAL(deloader_global_context_t, deloader_init_once);
+
+// Load Depth Engine
+static void deloader_init_once(deloader_global_context_t* global)
+{
+    // All members are initialized to zero
+
+    ////k4a_result_t result = dynlib_create(K4A_PLUGIN_DYNAMIC_LIBRARY_NAME, K4A_PLUGIN_VERSION, &global->handle);
+    //if (K4A_FAILED(result))
+    //{
+    //    //LOG_ERROR("Failed to Load Depth Engine Plugin (%s). Depth functionality will not work",
+    //    //    K4A_PLUGIN_DYNAMIC_LIBRARY_NAME);
+    //    //LOG_ERROR("Make sure the depth engine plugin is in your loaders path", 0);
+    //}
+
+    //if (K4A_SUCCEEDED(result))
+    //{
+    //    result = dynlib_find_symbol(global->handle, K4A_PLUGIN_EXPORTED_FUNCTION, (void**)&global->registerFn);
+    //}
+
+    //if (K4A_SUCCEEDED(result))
+    //{
+    //    //result = K4A_RESULT_FROM_BOOL(global->registerFn(&global->plugin));
+    //    global->registerFn(&global->plugin);
+    //}
+
+    //if (K4A_SUCCEEDED(result))
+    //{
+    //    //result = K4A_RESULT_FROM_BOOL(verify_plugin(&global->plugin));
+    //}
+
+    //if (K4A_SUCCEEDED(result))
+    //{
+    //    global->loaded = true;
+    //}
+}
+
+static bool is_plugin_loaded(deloader_global_context_t* global)
+{
+    return global->loaded;
+}
+
+
+void deloader_deinit(void)
+{
+    deloader_global_context_t* global = deloader_global_context_t_get();
+
+    if (global->handle)
+    {
+        dynlib_destroy(global->handle);
+    }
+}
+
+K4aLoader::K4aLoader(std::string dll, deloader_global_context_t* global)
+{
+    printf("Start loading dynamic libraries.\n");
+    k4a_result_t result = dynlib_create(dll.c_str(), &global->handle);
+    if (K4A_SUCCEEDED(result))
+    {
+        printf("Dynamic library loaded.\n");
+    }
+    else
+    {
+        printf("Dynamic library loading failure.\n");
+    }
+
+    printf("Start initializing function pointers.\n");
+    if (K4A_SUCCEEDED(result))
+    {
+        result = dynlib_find_symbol(global->handle, "k4a_device_get_installed_count", (void**)&functionList.k4a_device_get_installed_count);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_installed_count function failed to load.\n");
+        }
+    }
+    printf("Function pointer initialization complete.\n");
+
+    if (K4A_SUCCEEDED(result))
+    {
+        global->loaded = true;
+    }
+    
+}
+
+K4aLoader::~K4aLoader() 
+{
+    deloader_deinit();
+}
---
+++ dynlib_windows.c
@@ -0,0 +1,145 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This library
+#include "dynlib.h"
+#include "handle.h"
+
+// Dependent libraries
+
+// System dependencies
+#include <windows.h>
+#include <stdio.h>
+#include <pathcch.h>
+
+#define K4A_RESULT_FROM_BOOL(call) (call) ? K4A_RESULT_SUCCEEDED : K4A_RESULT_FAILED
+
+#define TOSTRING(x) STRINGIFY(x)
+#define VOID_VALUE
+
+typedef struct _dynlib_context_t {
+    HMODULE handle;
+} dynlib_context_t;
+
+K4A_DECLARE_CONTEXT(dynlib_t, dynlib_context_t);
+
+//static char *generate_file_name(const char *name, uint32_t version) {
+static char *generate_file_orbbec_name(const char *name) {
+    // Format of the depth engine name is: depthengine_<K4A_PLUGIN_VERSION>_0
+    //                                     depthengine_2_0
+    //size_t max_buffer_size = strlen(name) + strlen(TOSTRING(DYNLIB_MAX_VERSION)) + strlen("_0") + strlen("_") + 1;
+    size_t max_buffer_size = strlen(name) + strlen("\\") + strlen("OrbbecSDK.dll") + strlen("_") + 1;
+
+    char *versioned_file_name = malloc(max_buffer_size);
+    if(versioned_file_name == NULL) {
+        // LOG_ERROR("malloc failed with size %llu", max_buffer_size);
+        return NULL;
+    }
+    versioned_file_name[0] = '\0';
+
+    // NOTE: _0 is appended to the name for legacy reasons, a time when the depth engine plugin version was tracked with
+    // major and minor versions
+    //snprintf(versioned_file_name, max_buffer_size, "%s_%u_0", name, version);
+    snprintf(versioned_file_name, max_buffer_size, "%s\\OrbbecSDK.dll", name);
+
+    return versioned_file_name;
+}
+
+static char* generate_file_k4a_name(const char* name) {
+    // Format of the depth engine name is: depthengine_<K4A_PLUGIN_VERSION>_0
+    //                                     depthengine_2_0
+    //size_t max_buffer_size = strlen(name) + strlen(TOSTRING(DYNLIB_MAX_VERSION)) + strlen("_0") + strlen("_") + 1;
+    size_t max_buffer_size = strlen(name) + strlen("\\") + strlen("k4a.dll") + strlen("_") + 1;
+
+    char* versioned_file_name = malloc(max_buffer_size);
+    if (versioned_file_name == NULL) {
+        // LOG_ERROR("malloc failed with size %llu", max_buffer_size);
+        return NULL;
+    }
+    versioned_file_name[0] = '\0';
+
+    // NOTE: _0 is appended to the name for legacy reasons, a time when the depth engine plugin version was tracked with
+    // major and minor versions
+    //snprintf(versioned_file_name, max_buffer_size, "%s_%u_0", name, version);
+    snprintf(versioned_file_name, max_buffer_size, "%s\\k4a.dll", name);
+
+    return versioned_file_name;
+}
+
+//k4a_result_t dynlib_create(const char *name, uint32_t version, dynlib_t *dynlib_handle) {
+k4a_result_t dynlib_create(const char *name, dynlib_t *dynlib_handle) {
+    if (name == NULL)
+    {
+        printf("dll folder path is empty.\n");
+        return K4A_RESULT_FAILED;
+    }
+
+    char* versioned_orbbec_name = generate_file_orbbec_name(name);
+    char *versioned_k4a_name = generate_file_k4a_name(name);
+    if(versioned_orbbec_name == NULL && versioned_k4a_name == NULL) {
+        return K4A_RESULT_FAILED;
+    }
+
+    dynlib_context_t *dynlib = dynlib_t_create(dynlib_handle);
+    k4a_result_t      result = K4A_RESULT_FROM_BOOL(dynlib != NULL);
+
+    if(K4A_SUCCEEDED(result)) {
+        HMODULE hanlde = LoadLibraryA(versioned_orbbec_name, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_USER_DIRS);
+        result         = (hanlde != NULL) ? K4A_RESULT_SUCCEEDED : K4A_RESULT_FAILED;
+        if (hanlde){
+            dynlib->handle = LoadLibraryA(versioned_k4a_name, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_USER_DIRS);
+            result = (dynlib->handle != NULL) ? K4A_RESULT_SUCCEEDED : K4A_RESULT_FAILED;
+        }
+        else
+        {
+            printf("Failed to load dll for OrbbecSDK.\n");
+        }
+    }
+
+    if(versioned_orbbec_name != NULL) {
+        free(versioned_orbbec_name);
+    }
+
+    if (versioned_k4a_name != NULL) {
+        free(versioned_k4a_name);
+    }
+
+    if(K4A_FAILED(result)) {
+        dynlib_t_destroy(*dynlib_handle);
+        *dynlib_handle = NULL;
+    }
+
+    return result;
+}
+
+k4a_result_t dynlib_find_symbol(dynlib_t dynlib_handle, const char *symbol, void **address) {
+
+    k4a_result_t      result = K4A_RESULT_SUCCEEDED;
+    dynlib_context_t *dynlib = dynlib_t_get_context(dynlib_handle);
+
+    void *ptr = (void *)GetProcAddress(dynlib->handle, symbol);
+    result    = K4A_RESULT_FROM_BOOL(ptr != NULL);
+
+    if(K4A_SUCCEEDED(result)) {
+        *address = ptr;
+    }
+    else {
+        printf("Failed to find symbol %s in dynamic library, GLE is 0x%08x", symbol, GetLastError());
+    }
+
+    return result;
+}
+
+void dynlib_destroy(dynlib_t dynlib_handle) {
+    // RETURN_VALUE_IF_HANDLE_INVALID(VOID_VALUE, dynlib_t, dynlib_handle);
+
+    dynlib_context_t *dynlib = dynlib_t_get_context(dynlib_handle);
+
+    BOOL freed = FreeLibrary(dynlib->handle);
+    if(!freed) {
+        // LOG_ERROR("Failed to unload dynamic library");
+    }
+
+    dynlib_t_destroy(dynlib_handle);
+    dynlib = NULL;
+}
