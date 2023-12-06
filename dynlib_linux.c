a5be26e0579d07f05a383d76f2a0af79b829459c
lingyi <lingyi@orbbec.com>
2023-12-06 11:21:48 +0800

    [k4a][fixed]: Adapts to dynamic library loading under Linux

--- dynlib_linux.c
+++ dynlib_linux.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#define _GNU_SOURCE  // ldaddr() extention in dlfcn.h
+#define _GNU_SOURCE // ldaddr() extention in dlfcn.h
 
 // This library
 #include "dynlib.h"
@@ -17,112 +17,146 @@
 
 #define TOSTRING(x) STRINGIFY(x)
 
-typedef struct _dynlib_context_t {
+typedef struct _dynlib_context_t
+{
     void *handle;
 } dynlib_context_t;
 
 K4A_DECLARE_CONTEXT(dynlib_t, dynlib_context_t);
-//static char *generate_file_name(const char *name, uint32_t version) {
-static char *generate_file_name(const char *name) {
+// static char *generate_file_name(const char *name, uint32_t version) {
+static char *generate_file_orbbec_name(const char *name)
+{
     const char *lib_prefix = "lib";
     const char *lib_suffix = "so";
-    // Format of the depth engine name is: libdepthengine.so.<K4A_PLUGIN_VERSION>.0
-    //                                     libdepthengine.so.2.0
+
     size_t max_buffer_size =
-        //strlen(name) + strlen(TOSTRING(DYNLIB_MAX_VERSION)) + strlen(".0") + strlen(".") + strlen(".") + strlen(lib_suffix) + strlen(lib_prefix) + 1;
-        strlen(name) + strlen(".0") + strlen(".") + strlen(".") + strlen(lib_suffix) + strlen(lib_prefix) + 1;
+        strlen(name) + strlen("/") + strlen(".") + strlen(lib_suffix) + strlen("libOrbbecSDK") + 1;
 
     char *versioned_file_name = malloc(max_buffer_size);
-    if(versioned_file_name == NULL) {
+    if (versioned_file_name == NULL)
+    {
         // LOG_ERROR("malloc failed with size %llu", max_buffer_size);
         return NULL;
     }
     versioned_file_name[0] = '\0';
     // NOTE: 0 is appended to the name for legacy reasons, a time when the depth engine plugin version was tracked with
     // major and minor versions
-    //snprintf(versioned_file_name, max_buffer_size, "%s%s.%s.%u.0", lib_prefix, name, lib_suffix, version);
-    snprintf(versioned_file_name, max_buffer_size, "%s%s.%s.0", lib_prefix, name, lib_suffix);
-
+    // snprintf(versioned_file_name, max_buffer_size, "%s%s.%s.%u.0", lib_prefix, name, lib_suffix, version);
+    snprintf(versioned_file_name, max_buffer_size, "%s/libOrbbecSDK.%s", name, lib_suffix);
+    printf("%s", versioned_file_name);
     return versioned_file_name;
 }
 
-//k4a_result_t dynlib_create(const char *name, uint32_t version, dynlib_t *dynlib_handle) {
-k4a_result_t dynlib_create(const char *name, dynlib_t *dynlib_handle) {
-    // Note: A nullptr is allowed on linux systems for "name", however, this is
-    // functionality we do not support
-    // RETURN_VALUE_IF_ARG(K4A_RESULT_FAILED, name == NULL);
-    // RETURN_VALUE_IF_ARG(K4A_RESULT_FAILED, dynlib_handle == NULL);
+static char *generate_file_k4a_name(const char *name)
+{
+    const char *lib_prefix = "lib";
+    const char *lib_suffix = "so";
 
-    //if(version > DYNLIB_MAX_VERSION) {
-    //    // LOG_ERROR("Failed to load dynamic library %s. version %u is too large to load. Max is %u\n",
-    //    //    name,
-    //    //    version,
-    //    //    DYNLIB_MAX_VERSION);
-    //    return K4A_RESULT_FAILED;
-    //}
+    size_t max_buffer_size =
+        strlen(name) + strlen("/") + strlen(".") + strlen(lib_suffix) + strlen("libk4a") + 1;
 
-    char* versioned_name = generate_file_name(name, version);
-    char *versioned_name = generate_file_name(name);
-    if(versioned_name == NULL) {
+    char *versioned_file_name = malloc(max_buffer_size);
+    if (versioned_file_name == NULL)
+    {
+        // LOG_ERROR("malloc failed with size %llu", max_buffer_size);
+        return NULL;
+    }
+    versioned_file_name[0] = '\0';
+    // NOTE: 0 is appended to the name for legacy reasons, a time when the depth engine plugin version was tracked with
+    // major and minor versions
+    // snprintf(versioned_file_name, max_buffer_size, "%s%s.%s.%u.0", lib_prefix, name, lib_suffix, version);
+    snprintf(versioned_file_name, max_buffer_size, "%s/libk4a.%s", name, lib_suffix);
+
+    return versioned_file_name;
+}
+// k4a_result_t dynlib_create(const char *name, uint32_t version, dynlib_t *dynlib_handle) {
+k4a_result_t dynlib_create(const char *name, dynlib_t *dynlib_handle)
+{
+    if (name == NULL)
+    {
+        printf("so folder path is empty.\n");
+        return K4A_RESULT_FAILED;
+    }
+
+    char *versioned_orbbec_name = generate_file_orbbec_name(name);
+    char *versioned_k4a_name = generate_file_k4a_name(name);
+    if (versioned_orbbec_name == NULL && versioned_k4a_name == NULL)
+    {
         return K4A_RESULT_FAILED;
     }
 
     dynlib_context_t *dynlib = dynlib_t_create(dynlib_handle);
-    k4a_result_t      result = K4A_RESULT_FROM_BOOL(dynlib != NULL);
+    k4a_result_t result = K4A_RESULT_FROM_BOOL(dynlib != NULL);
 
-    if(K4A_SUCCEEDED(result)) {
-        dynlib->handle = dlopen(versioned_name, RTLD_NOW);
-
+    if (K4A_SUCCEEDED(result))
+    {
+        auto handle = dlopen(versioned_orbbec_name, RTLD_NOW);
         result = (dynlib->handle != NULL) ? K4A_RESULT_SUCCEEDED : K4A_RESULT_FAILED;
 
-        if(K4A_FAILED(result)) {
-            // LOG_ERROR("Failed to load shared object %s with error: %s", versioned_name, dlerror());
+        if (handle)
+        {
+            dynlib->handle = dlopen(versioned_k4a_name, RTLD_NOW);
+            result = (dynlib->handle != NULL) ? K4A_RESULT_SUCCEEDED : K4A_RESULT_FAILED;
+        }
+        else
+        {
+            printf("Failed to load shared with error: %s\n", dlerror());
         }
     }
 
-    if(versioned_name) {
-        free(versioned_name);
+    if (versioned_orbbec_name != NULL)
+    {
+        free(versioned_orbbec_name);
     }
 
-    if(K4A_FAILED(result)) {
+    if (versioned_k4a_name != NULL)
+    {
+        free(versioned_k4a_name);
+    }
+
+    if (K4A_FAILED(result))
+    {
         dynlib_t_destroy(*dynlib_handle);
         *dynlib_handle = NULL;
     }
     return result;
 }
 
-k4a_result_t dynlib_find_symbol(dynlib_t dynlib_handle, const char *symbol, void **address) {
-    // RETURN_VALUE_IF_HANDLE_INVALID(K4A_RESULT_FAILED, dynlib_t, dynlib_handle);
-    // RETURN_VALUE_IF_ARG(K4A_RESULT_FAILED, symbol == NULL);
-    // RETURN_VALUE_IF_ARG(K4A_RESULT_FAILED, address == NULL);
-
-    k4a_result_t      result = K4A_RESULT_SUCCEEDED;
+k4a_result_t dynlib_find_symbol(dynlib_t dynlib_handle, const char *symbol, void **address)
+{
+    k4a_result_t result = K4A_RESULT_SUCCEEDED;
     dynlib_context_t *dynlib = dynlib_t_get_context(dynlib_handle);
 
     void *ptr = dlsym(dynlib->handle, symbol);
-    result    = K4A_RESULT_FROM_BOOL(ptr != NULL);
+    result = K4A_RESULT_FROM_BOOL(ptr != NULL);
 
-    if(K4A_SUCCEEDED(result)) {
+    if (K4A_SUCCEEDED(result))
+    {
         *address = ptr;
     }
-    else {
-        // LOG_ERROR("Failed to find symbol %s in dynamic library. Error: ", symbol, dlerror());
+    else
+    {
+        printf("Failed to find symbol %s in dynamic library. Error: %s\n", symbol, dlerror());
     }
 
-    if(K4A_SUCCEEDED(result)) {
+    if (K4A_SUCCEEDED(result))
+    {
         Dl_info info;
-        if(dladdr(*address, &info) != 0) {
-            // LOG_INFO("Depth Engine loaded %s", info.dli_fname);
+        if (dladdr(*address, &info) != 0)
+        {
+            printf("Depth Engine loaded %s\n", info.dli_fname);
         }
-        else {
-            // LOG_ERROR("Failed calling dladdr %x", dlerror());
+        else
+        {
+            printf("Failed calling dladdr %x\n", dlerror());
         }
     }
 
     return result;
 }
 
-void dynlib_destroy(dynlib_t dynlib_handle) {
+void dynlib_destroy(dynlib_t dynlib_handle)
+{
 
     dynlib_context_t *dynlib = dynlib_t_get_context(dynlib_handle);
 
