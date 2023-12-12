9ef9ca4629425bcdbe91f2f541a65f171f9e53ab
lingyi <lingyi@orbbec.com>
2023-12-12 15:12:14 +0800

    [k4aloader][fixed]: 补全函数指针初始化。

--- K4aLoader.cpp
+++ K4aLoader.cpp
@@ -36,6 +36,16 @@ k4aloader_handle* k4aloader_dll_file_load(const char* dll)
         {
             printf("k4a_device_get_installed_count function failed to load.\n");
         }
+        result = dynlib_find_symbol(hdl, "k4a_set_debug_message_handler", (void**)&instance->k4a_set_debug_message_handler);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_set_debug_message_handler function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_set_allocator", (void**)&instance->k4a_set_allocator);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_set_allocator function failed to load.\n");
+        }
         result = dynlib_find_symbol(hdl, "k4a_device_get_calibration", (void **)&instance->k4a_device_get_calibration);
         if (K4A_FAILED(result))
         {
@@ -50,6 +60,306 @@ k4aloader_handle* k4aloader_dll_file_load(const char* dll)
         if (K4A_FAILED(result))
         {
             printf("k4a_device_close function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_capture", (void**)&instance->k4a_device_get_capture);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_capture function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_imu_sample", (void**)&instance->k4a_device_get_imu_sample);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_imu_sample function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_create", (void**)&instance->k4a_capture_create);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_create function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_release", (void**)&instance->k4a_capture_release);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_release function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_reference", (void**)&instance->k4a_capture_reference);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_reference function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_get_color_image", (void**)&instance->k4a_capture_get_color_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_get_color_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_get_depth_image", (void**)&instance->k4a_capture_get_depth_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_get_depth_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_get_ir_image", (void**)&instance->k4a_capture_get_ir_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_get_ir_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_set_color_image", (void**)&instance->k4a_capture_set_color_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_set_color_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_set_depth_image", (void**)&instance->k4a_capture_set_depth_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_set_depth_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_set_ir_image", (void**)&instance->k4a_capture_set_ir_image);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_set_ir_image function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_set_temperature_c", (void**)&instance->k4a_capture_set_temperature_c);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_set_temperature_c function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_capture_get_temperature_c", (void**)&instance->k4a_capture_get_temperature_c);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_capture_get_temperature_c function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_create", (void**)&instance->k4a_image_create);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_create function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_create_from_buffer", (void**)&instance->k4a_image_create_from_buffer);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_create_from_buffer function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_buffer", (void**)&instance->k4a_image_get_buffer);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_buffer function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_size", (void**)&instance->k4a_image_get_size);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_size function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_format", (void**)&instance->k4a_image_get_format);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_format function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_width_pixels", (void**)&instance->k4a_image_get_width_pixels);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_width_pixels function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_height_pixels", (void**)&instance->k4a_image_get_height_pixels);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_height_pixels function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_stride_bytes", (void**)&instance->k4a_image_get_stride_bytes);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_stride_bytes function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_timestamp_usec", (void**)&instance->k4a_image_get_timestamp_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_timestamp_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_device_timestamp_usec", (void**)&instance->k4a_image_get_device_timestamp_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_device_timestamp_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_system_timestamp_nsec", (void**)&instance->k4a_image_get_system_timestamp_nsec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_system_timestamp_nsec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_exposure_usec", (void**)&instance->k4a_image_get_exposure_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_exposure_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_white_balance", (void**)&instance->k4a_image_get_white_balance);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_white_balance function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_get_iso_speed", (void**)&instance->k4a_image_get_iso_speed);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_get_iso_speed function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_device_timestamp_usec", (void**)&instance->k4a_image_set_device_timestamp_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_device_timestamp_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_timestamp_usec", (void**)&instance->k4a_image_set_timestamp_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_timestamp_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_system_timestamp_nsec", (void**)&instance->k4a_image_set_system_timestamp_nsec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_system_timestamp_nsec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_exposure_usec", (void**)&instance->k4a_image_set_exposure_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_exposure_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_exposure_time_usec", (void**)&instance->k4a_image_set_exposure_time_usec);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_exposure_time_usec function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_white_balance", (void**)&instance->k4a_image_set_white_balance);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_white_balance function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_set_iso_speed", (void**)&instance->k4a_image_set_iso_speed);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_set_iso_speed function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_reference", (void**)&instance->k4a_image_reference);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_reference function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_image_release", (void**)&instance->k4a_image_release);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_image_release function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_start_cameras", (void**)&instance->k4a_device_start_cameras);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_start_cameras function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_stop_cameras", (void**)&instance->k4a_device_stop_cameras);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_stop_cameras function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_start_imu", (void**)&instance->k4a_device_start_imu);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_start_imu function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_stop_imu", (void**)&instance->k4a_device_stop_imu);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_stop_imu function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_serialnum", (void**)&instance->k4a_device_get_serialnum);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_serialnum function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_version", (void**)&instance->k4a_device_get_version);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_version function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_color_control_capabilities", (void**)&instance->k4a_device_get_color_control_capabilities);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_color_control_capabilities function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_color_control", (void**)&instance->k4a_device_get_color_control);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_color_control function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_set_color_control", (void**)&instance->k4a_device_set_color_control);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_set_color_control function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_raw_calibration", (void**)&instance->k4a_device_get_raw_calibration);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_raw_calibration function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_calibration", (void**)&instance->k4a_device_get_calibration);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_calibration function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_device_get_sync_jack", (void**)&instance->k4a_device_get_sync_jack);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_device_get_sync_jack function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_get_from_raw", (void**)&instance->k4a_calibration_get_from_raw);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_get_from_raw function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_3d_to_3d", (void**)&instance->k4a_calibration_3d_to_3d);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_3d_to_3d function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_2d_to_3d", (void**)&instance->k4a_calibration_2d_to_3d);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_2d_to_3d function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_3d_to_2d", (void**)&instance->k4a_calibration_3d_to_2d);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_3d_to_2d function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_2d_to_2d", (void**)&instance->k4a_calibration_2d_to_2d);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_2d_to_2d function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_calibration_color_2d_to_depth_2d", (void**)&instance->k4a_calibration_color_2d_to_depth_2d);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_calibration_color_2d_to_depth_2d function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_create", (void**)&instance->k4a_transformation_create);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_create function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_destroy", (void**)&instance->k4a_transformation_destroy);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_destroy function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_depth_image_to_color_camera", (void**)&instance->k4a_transformation_depth_image_to_color_camera);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_depth_image_to_color_camera function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_depth_image_to_color_camera_custom", (void**)&instance->k4a_transformation_depth_image_to_color_camera_custom);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_depth_image_to_color_camera_custom function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_color_image_to_depth_camera", (void**)&instance->k4a_transformation_color_image_to_depth_camera);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_color_image_to_depth_camera function failed to load.\n");
+        }
+        result = dynlib_find_symbol(hdl, "k4a_transformation_depth_image_to_point_cloud", (void**)&instance->k4a_transformation_depth_image_to_point_cloud);
+        if (K4A_FAILED(result))
+        {
+            printf("k4a_transformation_depth_image_to_point_cloud function failed to load.\n");
         }
     }
     printf("Function pointer initialization complete.\n");
